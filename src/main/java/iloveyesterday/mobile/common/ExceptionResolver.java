d74d19f095f82d7d05eec12f9760757efecfaa8f
zhaoshengzhi <iloveyesterday@outlook.com>
2018-05-07 09:48:22 +0800

    log

---
+++ src/main/java/iloveyesterday/mobile/common/ExceptionResolver.java
@@ -0,0 +1,26 @@
+package iloveyesterday.mobile.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
+public class ExceptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+        // 记录到日志
+        log.error("{} Exception", httpServletRequest.getRequestURI(), e);
+        // 返回json数据
+        ModelAndView modelAndView = new ModelAndView(new MappingJackson2JsonView());
+        modelAndView.addObject("status", ResponseCode.ERROR.getCode());
+        modelAndView.addObject("msg", "请查看日志");
+        modelAndView.addObject("data", e.toString());
+        return modelAndView;
+    }
+}
